b61a26a84149b5c1927e65f29980947dc50a485a
brown-cody <xrkund@yahoo.com>
2018-03-26 11:38:32 -0700

    Style for Recipe Add Page

--- admin/view/addrecipeview.php
+++ admin/view/addrecipeview.php
@@ -3,15 +3,15 @@
     <h2>Add Recipe</h2>
     
 
-    <form action="index.php" method="post">
+    <form action="index.php" method="post" class="recipeAddForm">
         <input type="hidden" name="action" value="saveaddrecipe">
         
-        <label>Recipe Name:</label>
-        <input type="text" name="recipeName" required>
+        <label for="recipeName" class="formLabel">Recipe Name:</label>
+        <input type="text" id="recipeName" name="recipeName" class="formInput" required>
         <br>
 
-        <label for="category">Category</label>
-        <select id="category" name="recipeCategory">
+        <label for="category" class="formLabel">Category:</label>
+        <select id="category" name="recipeCategory" class="formInput">
             <?php foreach($categories as $category): ?>
                 <option value="<?php echo $category['categoryID']; ?>">
                     <?php echo $category['categoryName']; ?>
@@ -20,27 +20,21 @@
         </select>
         <br>
 
-        <label for="contributor">Contributor:</label>
-        <input type="text" name="recipeContributor" required>
+        <label for="recipeContributor" class="formLabel">Contributor:</label>
+        <input type="text" id="recipeContributor" name="recipeContributor" class="formInput" required>
         <br>        
-        
-        <!-- OLD IMAGE ADD
-        <label for="image">Image:</label>
-        <input type="text" name="recipeImage">.jpg
-        <br>
-        -->
 
-        <label for="ingredients">Ingredients (separate with commas):</label>
-        <input type="text" name="recipeIngredients" required>
+        <label for="recipeIngredients" class="formLabel">Ingredients (separate with commas):</label>
+        <input type="text" id="recipeIngredients" name="recipeIngredients" class="formInput" required>
         <br>
 
-        <label for="instructions">Instructions:</label>
-        <textarea rows="10" cols="30" name="recipeInstructions" required></textarea>
+        <label for="recipeInstructions" class="formLabel">Instructions:</label>
+        <textarea rows="10" cols="30" id="recipeInstructions" name="recipeInstructions" class="formInput" required></textarea>
         <br>
 
-        <button type="submit" class="mainButton">Submit</button>
+        <button type="submit" class="mainButton submitButton">Submit</button>
     </form>
-
+    <br>
     <form action="index.php" method="post">
         <input type="hidden" name="action" value="adminview">
                 
